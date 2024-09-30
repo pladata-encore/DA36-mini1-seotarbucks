8c2524376faa8b1742d212bb770d79cfa0b73fa8
YooYoungJun-3 <yooyjun@gmail.com>
2024-09-30 15:20:57 +0900

    Feat : 메뉴

---
+++ kiosk/_01_menu.py
@@ -0,0 +1,21 @@
+# Coffee 카테고리
+kiosk.add_menu_item(Beverage("아메리카노", 4500, "Coffee"))
+kiosk.add_menu_item(Beverage("카페라떼", 5000, "Coffee"))
+kiosk.add_menu_item(Beverage("돌체라떼", 5900, "Coffee"))
+kiosk.add_menu_item(Beverage("카페모카", 5500, "Coffee"))
+kiosk.add_menu_item(Beverage("바닐라라떼", 5500, "Coffee"))
+kiosk.add_menu_item(Beverage("카라멜마키아토", 5900, "Coffee"))
+kiosk.add_menu_item(Beverage("플랫화이트", 5500, "Coffee"))
+
+# Non-Caffeine 카테고리
+kiosk.add_menu_item(Beverage("허니 레몬티", 5000, "Non-Caffeine"))
+kiosk.add_menu_item(Beverage("유자차", 5500, "Non-Caffeine"))
+kiosk.add_menu_item(Beverage("피지오", 5900, "Non-Caffeine"))
+kiosk.add_menu_item(Beverage("초코", 5500, "Non-Caffeine"))
+kiosk.add_menu_item(Beverage("오렌지쥬스", 5000, "Non-Caffeine"))
+
+# Blended 카테고리
+kiosk.add_menu_item(Beverage("망고 블렌디드", 6000, "Blended"))
+kiosk.add_menu_item(Beverage("딸기 요거트 블렌디드", 6500, "Blended"))
+kiosk.add_menu_item(Beverage("제주그린티 프라프치노", 6800, "Blended"))
+kiosk.add_menu_item(Beverage("자바칩 프라프치노", 7000, "Blended"))
